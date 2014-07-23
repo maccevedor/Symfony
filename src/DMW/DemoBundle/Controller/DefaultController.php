ae676f956d3f9964b97b0037a59e6ed1e182aa7d
maccevedor <maccevedor@gmail.com>
2014-07-23 11:59:45 -0500

    terminado el modulo de creacion de paginas

--- src/DMW/DemoBundle/Controller/DefaultController.php
+++ src/DMW/DemoBundle/Controller/DefaultController.php
@@ -10,4 +10,38 @@ class DefaultController extends Controller
     {
         return $this->render('DMWDemoBundle:Default:index.html.twig', array('name' => $name));
     }
+    public function articulosAction()
+{
+    //-- Simulamos obtener los datos de la base de datos cargando los artículos a un array
+    $articulos = array(
+        array('id' => 1, 'title' => 'Articulo numero 1', 'created' => '2011-01-01'),
+        array('id' => 2, 'title' => 'Articulo numero 2', 'created' => '2011-01-01'),
+        array('id' => 3, 'title' => 'Articulo numero 3', 'created' => '2011-01-01'),
+    );
+    return $this->render('DMWDemoBundle:Default:articulos.html.twig', array('articulos' => $articulos));
 }
+    public function articuloAction($id)
+{
+    //-- Simulamos obtener los datos de la base de datos cargando los artículos a un array
+    $articulos = array(
+        array('id' => 1, 'title' => 'Articulo numero 1', 'created' => '2011-01-01'),
+        array('id' => 2, 'title' => 'Articulo numero 2', 'created' => '2011-01-01'),
+        array('id' => 3, 'title' => 'Articulo numero 3', 'created' => '2011-01-01'),
+    );
+
+    //-- Buscamos dentro del array el ID solicitado
+    $articuloSeleccionado = null;
+    foreach($articulos as $articulo)
+    {
+        if($articulo['id'] == $id)
+        {
+            $articuloSeleccionado = $articulo;
+            break;
+        }
+    }
+
+    //-- Invocamos a nuestra nueva plantilla, pasando los datos
+    return $this->render('DMWDemoBundle:Default:articulo.html.twig', array('articulo' => $articuloSeleccionado));
+ }
+}
+

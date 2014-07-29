9affaadf1a64b2b3838781d5623c5fe5ba22a901
maccevedor <maccevedor@gmail.com>
2014-07-29 09:24:39 -0500

    se movieron todas las routing.yml a app/config y se finalizo al validacion correctamente

--- src/DMW/DemoBundle/Controller/ArticulosController.php
+++ src/DMW/DemoBundle/Controller/ArticulosController.php
@@ -18,7 +18,7 @@ class ArticulosController extends Controller
         return $this->render('DMWDemoBundle:Articulos:listar.html.twig', array('articulos' => $articulos));
 
 	}
-	public function newAction()
+	public function newsAction()
 	{
     //-- Obtenemos el request que contendrá los datos
     $request = $this->getRequest();
@@ -29,10 +29,49 @@ class ArticulosController extends Controller
     //-- En caso de que el request haya sido invocado por POST
     //   procesaremos el formulario
         //return $this->redirect($this->generateURL('articulos'));
+        
         return $this->render('DMWDemoBundle:Articulos:new.html.twig', array(
         'form' => $form->createView(),
     ));
 	}
+    public function newAction()
+{
+    //-- Obtenemos el request que contendrá los datos
+    $request = $this->getRequest();
+
+    $articulo = new Articles();
+    $form = $this->createForm(new ArticleType(), $articulo);
+
+    //-- En caso de que el request haya sido invocado por POST
+    //   procesaremos el formulario
+    if($request->getMethod() == 'POST')
+    {
+        //-- Pasamos el request el método bindRequest() del objeto 
+        //   formulario el cual obtiene los datos del formulario
+        //   y los carga dentro del objeto Article que está contenido
+        //   también dentro del objeto Type
+        $form->bind($request);
+
+        //-- Con esto nuestro formulario ya es capaz de decirnos si
+        //   los dato son válidos o no y en caso de ser así
+        if($form->isValid())
+        {
+            //-- Procesamos los datos que ya están automáticamente
+            //   cargados dentro de nuestra variable $articulo, ya sea
+            //   grabándolos en la base de datos, enviando un mail, etc
+
+            //-- Finalmente, al finalizar el procesamiento, siempre es
+            //   importante realizar una redirección para no tener el
+            //   problema de que al intentar actualizar el navegador
+            //   nos dice que lo datos se deben volver a reenviar. En
+            //   este caso iremos a la página del listado de artículos
+            return $this->redirect($this->generateURL('articulo_listar'));
+        }
+    }
+    return $this->render('DMWDemoBundle:Articulos:new.html.twig', array(
+        'form' => $form->createView(),
+    ));
+}
 	public function crearAction()
 	{
     $articulo = new Articles();
--- src/DMW/DemoBundle/Form/ArticleType.php
+++ src/DMW/DemoBundle/Form/ArticleType.php
@@ -1,16 +1,19 @@
 <?php
 namespace DMW\DemoBundle\Form;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-
-
+use Symfony\Component\Form\FormBuilderInterface; 
 class ArticleType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('title')
                 ->add('author')
-                ->add('created');
+                ->add('created')
+                ->add('updated')
+                ->add('slug')
+                ->add('category')
+                ->add('content')
+                ->add('tags');
     }
     public function getName()
     {
